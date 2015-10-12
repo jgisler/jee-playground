cf37685a6e1c8794a7d7cfbcde71585d2e081f8c
James Gisler <james.gisler@nike.com>
2015-10-12 15:58:46 -0700

    initial

---
+++ esb-gateway-war/src/main/java/org/gislers/playground/esb/gateway/config/GatewayApplication.java
@@ -0,0 +1,12 @@
+package org.gislers.playground.esb.gateway.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by jim
+ * Created on 10/4/15.
+ */
+@ApplicationPath("gateway")
+public class GatewayApplication extends Application {
+}
